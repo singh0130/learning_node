25ad9958873359190d7f979bb774db491701619a
singh0130 <mr.mohitsinghdev@gmail.com>
2022-05-08 07:56:19 +0530

    Kickstarting Node Js task2 solution

--- first-app.js
+++ first-app.js
@@ -1,3 +1,15 @@
 const fs=require('fs');
 fs.writeFileSync('hello.txt','Hello from Node.js');
 console.log('hello world');
+
+const prod= (a,b) => {
+    return a*b;
+}
+
+console.log(prod(5,2));
+
+const student={
+    name:'Mohit',
+    age:24
+}
+console.log(student.name);
