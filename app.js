5b7e2d4f4794306b16ad478ff1d5df48b9c1555d
singh0130 <mr.mohitsinghdev@gmail.com>
2022-05-19 21:44:20 +0530

    Express framework task1 solution

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require('http');
-const routes=require('./routes');
+//const http = require('http');
+//const routes=require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server=http.createServer(routes.handler);
+//console.log(routes.someText);
+const app= express();
 
-server.listen(3000);
+app.use((req,res,next) => {
+    console.log('In the middleware!');
+    next(); //Allows the request to continue to the next middleware in line
+});
+
+app.use((req,res,next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+//const server=http.createServer(app);
+
+//server.listen(3000);
+app.listen(3000);
