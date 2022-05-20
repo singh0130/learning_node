9b2e676fef138b3ea34996d8184db35b5dd84248
singh0130 <mr.mohitsinghdev@gmail.com>
2022-05-20 11:40:07 +0530

    express framework task2 solution

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 //const http = require('http');
 //const routes=require('./routes');
+const { application } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //console.log(routes.someText);
 const app= express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+/*
 app.use((req,res,next) => {
     console.log('In the middleware!');
     next(); //Allows the request to continue to the next middleware in line
 });
+*/
 
-app.use((req,res,next) => {
-    console.log('In another middleware!');
+/*
+app.use('/', (req,res,next) => {
+    console.log('This always runs!');
+    next();
+});
+*/
+
+app.use('/add-product', (req,res,next) => {
+    //console.log('In another middleware!');
+    //res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req,res,next) => {
+    //console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
