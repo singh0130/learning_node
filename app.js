72f21717c1155647f73c76d18e10a68756cacbff
singh0130 <mr.mohitsinghdev@gmail.com>
2022-05-20 22:47:43 +0530

    express framework task3 solution

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const { application } = require('express');
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 //console.log(routes.someText);
 const app= express();
@@ -23,20 +25,11 @@ app.use('/', (req,res,next) => {
 });
 */
 
-app.use('/add-product', (req,res,next) => {
-    //console.log('In another middleware!');
-    //res.send('<h1>The "Add Product" Page</h1>');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req,res,next) => {
-    //console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 //const server=http.createServer(app);
